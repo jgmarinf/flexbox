a270f6679b74051a09cff45e17236f6ce77a8fce
Juan Guillermo <jgmarinf93@gmail.com>
2022-11-21 16:06:27 -0500

    subiendo archivos

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+
+    </div>
+  )
+}
+
+export default App
